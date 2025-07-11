043916432a68f79277bd2585c07a9ebebd920775
Requiem2803 <shabba1165@gmail.com>
2025-07-11 09:18:57 +0100

    Portfolio V1.4.0

--- Portfolio/js/openCode.js
+++ Portfolio/js/openCode.js
@@ -1,11 +1,5 @@
-// const openCode = $(".open-code");
 
-// openCode.on("click", function() {
-//     $("main, nav").fadeOut(1000)
-    
-// });
-
-$(".change-page").on("click", function(e) {
+$(".open-code").on("click", function(e) {
     e.preventDefault();
     const linkLocation = this.href;
     $("body").animate(
